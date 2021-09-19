fcc8e6b08526039aab96c6776229b4527363134d
davidscdev <davidsc.dev@gmail.com>
2021-09-19 17:39:50 -0300

    Projeto comentado e finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-const labels = document.querySelectorAll('.form-control label');
+const labels = document.querySelectorAll('.form-control label'); //Seleciona todos os labels do form
 
 labels.forEach(label => {
     console.log('Texto de Entrada: ', label.innerText);
     label.innerHTML = label.innerText
-        .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-        .join('');
+        .split('') //Separa as letras do label
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) //Gera o HTML para cada span, permitindo que a animação possa ser feita por letra.
+        .join(''); //Junta os itens gerados no .map em uma única string 
 
     console.log('Label <span>: ', label.innerHTML);
 });
